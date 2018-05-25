10fabfd528d20e81e4b8b3fe215545f00d1e3adc
Oliver Evans <oliverevans96@gmail.com>
2018-05-25 19:44:32 -0400

    Rename testdata

--- test/src/TestParameters.cpp
+++ test/src/TestParameters.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 TEST_CASE("Parameters", "[lotus]") {
   int argc = 2;
-  char* argv[2] = {"test", "../test/run/test_config.yaml"};
+  char* argv[2] = {"test", "../test/data/test_config.yaml"};
   CommandLineParser commandLineParser(argc, argv);
   Options options = commandLineParser.options;
   REQUIRE(options.configPath == argv[1]);
